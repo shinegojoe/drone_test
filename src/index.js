545416490e3beb847e97337147064e1e1ab31ea0
taka <taka@huangguanghongdeMac-mini.local>
2021-05-05 13:40:37 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('this is index')
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://0.0.0.0:${port}`)
+})
